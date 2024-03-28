289bdeaff879922faeb107aed0ec0abef6e692d6
Samuel Prado <samuelsantanaprado@gmail.com>
2024-03-28 10:49:16 -0300

    atividade1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,7 +18,25 @@ void menu()
     Console.WriteLine("3. Avaliar: ");
     Console.WriteLine("4. Média Avaliação: ");
     Console.WriteLine("5. Sair: ");
+
+    Console.WriteLine("\nDigite sua opção: ");
+    string opcao = Console.ReadLine()!;
+    int opcaoN = int.Parse(opcao);
+    switch (opcaoN)
+    {
+        case 1: Console.WriteLine("Opção escolhida: " + opcaoN);
+            break; 
+        case 2: Console.WriteLine("Opção escolhida: " + opcaoN);
+            break;
+        case 3: Console.WriteLine("Opção escolhida: " + opcaoN);
+            break;
+        case 4: Console.WriteLine("Opção escolhida: " + opcaoN);
+            break;
+        case 5: Console.WriteLine("Opção escolhida: " + opcaoN);
+            break;
+        default: Console.WriteLine("Opção inválida!");
+            break;
+
+    }
 }
 
-msg();
-menu();
---
+++ ConsoleApp1/atividades.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class atividades
+    {
+        void atividade1()
+        {
+            // 1. Criar uma variável chamada notaMedia e atribua um valor inteiro a ela. Caso seu valor seja maior ou igual a 5, escreva na tela "Nota suficiente para aprovação".
+            string notaMedia = Console.ReadLine()!;
+            int notaMediaN = int.Parse(notaMedia);
+            if (notaMediaN >= 5)
+                Console.WriteLine("Nota suficiente para aprovação.");
+        }
+
+        void atividade2()
+        {
+            // 2. Criar uma lista de linguagens de programação, com as linguagens C#, Java e JavaScript.
+            List<string> linguagens = new List<string> { "C#", "Java", "JavaScript" };
+
+            // 3. Exibir o valor "C#" no console, utilizando a lista criada no exercício anterior.
+            Console.WriteLine(linguagens[0]);
+
+            // 4. Criar um programa que, dado a entrada de dados de um número n inteiro, a partir do teclado, exibir a n-ésima posição de uma lista.
+            Console.WriteLine("Digite um número para a posição na lista: ");
+            if (int.TryParse(Console.ReadLine(), out int n) && n >= 0 && n < linguagens.Count)
+                Console.WriteLine($"Posição {n}: {linguagens[n]}");
+            else
+                Console.WriteLine("Inválido!");
+
+        }
+    }
+}
