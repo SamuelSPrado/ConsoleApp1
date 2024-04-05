d9ee55b763d1543f90d99c6ad166ed9768370dbb
Samuel Prado <samuelsantanaprado@gmail.com>
2024-04-05 17:54:08 -0300

    !

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,11 @@
 ﻿
 //List<string> listaBandas = new List<string> { "U2", "Black Sabbath", "Iron Maiden", "Kiss" };
 
-Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>(); //Criando dicionário vazio
-bandasRegistradas.Add("U2", new List<int> { 10, 9, 5, 3, 8 });
-bandasRegistradas.Add("Link Park", new List<int>());
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>
+{
+    { "U2", new List<int> { 10, 9, 5, 3, 8 } },
+    { "Link Park", new List<int>() }
+};
 
 void Logo()
 {
@@ -18,11 +20,10 @@ void Logo()
     Console.WriteLine(msg);
     Thread.Sleep(1000);
 }
-
-void Load()
+static void Carregamento(string nomeCarregamento, int qntTempo)
 {
-    Console.WriteLine("\nCarregando menu de opções...\n");
-    Thread.Sleep(3000);
+    Console.WriteLine(nomeCarregamento);
+    Thread.Sleep(qntTempo);
     Console.Clear();
 }
 
@@ -43,7 +44,7 @@ void Menu()
     {
         case 1: Registrar(); break; 
         case 2: ListaBandas(); break;
-        case 3: Console.WriteLine("Opção escolhida: " + opcaoN); break;
+        case 3: Avaliar(); break;
         case 4: Console.WriteLine("Opção escolhida: " + opcaoN); break;
         case 5: Console.WriteLine("Opção escolhida: " + opcaoN); break;
         default: Console.WriteLine("Opção inválida!"); break;
@@ -57,9 +58,7 @@ void Registrar()
     Console.Write("Digite o nome da banda desejada: ");
     string nomeBanda = Console.ReadLine()!;
     bandasRegistradas.Add(nomeBanda, new List<int>());
-    Console.WriteLine($"Banda {nomeBanda} registrada com sucesso!");
-    Thread.Sleep(3000);
-    Console.Clear();
+    Carregamento($"Banda {nomeBanda} registrada com sucesso!", 3000);
     Menu();
 }
 
@@ -78,15 +77,46 @@ void ListaBandas()
         Console.WriteLine($"Banda: {banda}");
     }
 
-    Console.WriteLine("\n-- Voltar ao menu principal");
-    Console.ReadKey();
-    Console.Clear();
+    Carregamento("\n-- Voltar ao menu principal", 2000);
     Menu();
 }
 
-
 void Avaliar()
 {
+    Console.Clear();
+    ExibirTituloDaOpcao("Avalie sua banda!");
+    Console.Write("Digite o nome da banda que deseja avaliar: ");
+    string nomeBanda = Console.ReadLine()!;
+    if (bandasRegistradas.ContainsKey(nomeBanda))
+    {
+        Console.Write($"Atribua uma nota de 1 à 10 para a banda {nomeBanda}: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        if (nota < 0 || nota > 10)
+        {
+            Carregamento("\"Nota inválda! Deve ser entre 0 à 10\"", 2000);
+            Avaliar();
+        }
+        else
+        {
+            bandasRegistradas[nomeBanda].Add(nota);
+            Carregamento($"\nA nota {nota} foi registrada com sucesso para a banda {nomeBanda}", 3000);
+            Menu();
+        }
+    }
+    else
+    {
+        Console.WriteLine($"A banda {nomeBanda} não foi encontrada no registro. Deseja registrar?\n");
+        Console.WriteLine("1 - Sim\n2 - Ver Bandas Registradas\n3 - Voltar ao Menu principal");
+        string opcao = Console.ReadLine()!;
+        int opcaoN = int.Parse(opcao);
+        switch (opcaoN)
+        {
+            case 1: Registrar(); break;
+            case 2: ListaBandas(); break;
+            case 3: Menu(); break;
+            default: Console.WriteLine("Opção inválida!"); break;
+        }
+    }
 
 }
 
@@ -109,20 +139,62 @@ void ExibirTituloDaOpcao(string titulo)
     Console.WriteLine(asteriscos + "\n" + titulo + "\n" + asteriscos + "\n");
 }
 
-Menu();
-
-
-// Resolvendo ...
-
-List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-for (int i = 0; i < numeros.Count; i++)
+// Exercícios
+void VerificaPar()
 {
-    if (numeros[i] % 2 == 0) // Verifica se o número é par
-        Console.WriteLine(numeros[i]);
+    List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    for (int i = 0; i < numeros.Count; i++)
+    {
+        if (numeros[i] % 2 == 0) // Verifica se o número é par
+            Console.WriteLine(numeros[i]);
+    }
+
+    foreach (int numero in numeros)
+    {
+        if (numero % 2 == 0) Console.WriteLine(numero);
+    }
+} // Resolvendo ...
+
+void TirarMedia()
+{
+    Dictionary<string, List<double>> alunoNotas = new Dictionary<string, List<double>>
+        {
+            { "João", new List<double> { 7.5, 8.0, 9.5 } },
+            { "Maria", new List<double> { 6.0, 7.0, 8.0 } },
+            { "Pedro", new List<double> { 9.0, 9.5, 9.0 } }
+        };
+
+    foreach (var aluno in alunoNotas)
+    {
+        double media = aluno.Value.Average();
+        Console.WriteLine($"Média de notas de {aluno.Key}: {media}");
+    }
+} // Resolvendo 2 ...
+
+static void Estoque()
+{
+    // Dicionário para armazenar produtos e suas quantidades em estoque
+    Dictionary<string, int> estoque = new Dictionary<string, int>
+        {
+            { "cerveja", 254 },
+            { "biscoito", 159 }
+        };
+
+    // Solicitar ao usuário o nome do produto
+    Console.Write("Digite o nome do produto: ");
+    string nomeProduto = Console.ReadLine()!;
+
+    // Verificar se o produto está no estoque e exibir sua quantidade 'Dictionary<TKey, TValue>'
+    if (estoque.ContainsKey(nomeProduto))
+    {
+        int quantidade = estoque[nomeProduto];
+        Console.WriteLine($"Quantidade em estoque de {nomeProduto}: {quantidade}");
+    }
+    else
+    {
+        Console.WriteLine($"O produto '{nomeProduto}' não está em estoque.");
+    }
 }
 
-foreach (int numero in numeros)
-{
-    if (numero % 2 == 0) Console.WriteLine(numero);
-}
+// Executando
+Estoque();
