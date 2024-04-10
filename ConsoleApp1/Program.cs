5841660517ac4c4115d77d0906b6dba626084272
Samuel Prado <samuelsantanaprado@gmail.com>
2024-04-10 13:51:18 -0300

    Atividade complementar

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,4 @@
-﻿
-//List<string> listaBandas = new List<string> { "U2", "Black Sabbath", "Iron Maiden", "Kiss" };
+﻿//List<string> listaBandas = new List<string> { "U2", "Black Sabbath", "Iron Maiden", "Kiss" };
 
 Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>
 {
@@ -196,5 +195,21 @@ static void Estoque()
     }
 }
 
+//Atividade complementar
+static void VendasCarros()
+{
+    Dictionary<string, List<int>> vendasCarros = new Dictionary<string, List<int>> 
+    {
+        { "Bugatti Veyron", new List<int> { 10, 15, 12, 8, 5 } },
+        { "Koenigsegg Agera RS", new List<int> { 2, 3, 5, 6, 7 } },
+        { "Lamborghini Aventador", new List<int> { 20, 18, 22, 24, 16 } },
+        { "Pagani Huayra", new List<int> { 4, 5, 6, 5, 4 } },
+        { "Ferrari LaFerrari", new List<int> { 7, 6, 5, 8, 10 } }
+    };
+    //Calculando média
+    double mediaVendasBugatti = vendasCarros["Bugatti Veyron"].Average();
+    Console.WriteLine("Média de vendas do Bugatti Veyron: " + mediaVendasBugatti);
+}
+
 // Executando
-Estoque();
+VendasCarros();
